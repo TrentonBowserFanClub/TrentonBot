ddd6f93cb6fe4dc0b9c5c8a9c999c343ef90f6c1
NathanDuPont <nathan.dupont01@gmail.com>
2024-07-14 23:06:27 -0700

    Adding type alias

--- src/math/vector_math.h
+++ src/math/vector_math.h
@@ -1,6 +1,12 @@
 #include <Eigen/Eigen>
 
+// Struct to hold a position (as a vector from the origin) and a rotation of
+// an object in 2D space.
 struct Location {
   Eigen::Vector2d position;
   Eigen::Rotation2Df rotation;
 };
+
+// Movement and location are made of the same constituent parts, but are used
+// in different contexts. For now, just create a typedef alias.
+typedef Location Movement;
